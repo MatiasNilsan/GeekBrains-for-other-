67fd4775c38258c8a322c2d9f89ab10fdfc464dc
MatiasNilsan <72612296+MatiasNilsan@users.noreply.github.com>
2020-11-19 22:15:38 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
+
+//#1 и #3*
 const handleClick = (event) => {
     const src = event.target.getAttribute('src');
     const oldImg = document.querySelector('.full-picture-item');
 
-    if (oldImg && oldImg.getAttribute('src') === src) {
-        console.log("Stopped");
-        return;
-    }
-    
+    const leftArrow = document.createElement('button');
+    leftArrow.classList.add('left-arrow');
+    leftArrow.innerText = 'Previous';
+
+    const rightArrow = document.createElement('button');
+    rightArrow.classList.add('right-arrow')
+    rightArrow.innerText = 'Next';
+
+    leftArrow.addEventListener('click', leftArrowClick);
+    rightArrow.addEventListener('click', rightArrowClick);
+
     const wrapper = document.querySelector('.full-picture');
-    
+
     const img = document.createElement('img');
     img.classList.add("full-picture-item");
     img.setAttribute('src', src);
-    
+
+
+
     if (oldImg) {
         wrapper.replaceChild(img, oldImg);
-        console.log("Pucture changed");
+        console.log("Picture changed");
     } else {
+        wrapper.appendChild(leftArrow);
         wrapper.appendChild(img);
-        console.log("Pucture added");
+        wrapper.appendChild(rightArrow);
+        console.log("Picture added");
     }
+
+
+
 }
+
+const previews = document.querySelectorAll('img.preview-item');
 
 const handleMouseEnter = (event) => {
     console.log("Mouse entered!");
@@ -32,16 +49,73 @@ const handleMouseLeave = (event) => {
     event.target.classList.remove('active')
 }
 
+
+
 window.onload = () => {
-    // выбрать все .preview-item
-    // добавить им обработчик кликов
-    // взять src атрибут и 
-    // создать элемент img.full - picture - item внутри
-    // .full - picture
-    const previews = document.querySelectorAll('img.preview-item');
     for (const item of previews) {
         item.addEventListener('click', handleClick);
         item.addEventListener('mouseenter', handleMouseEnter);
         item.addEventListener('mouseleave', handleMouseLeave);
+
+    }
+
+}
+
+
+leftArrowClick = () => {
+    const oldImg = document.querySelector('.full-picture-item');
+    const wrapper = document.querySelector('.full-picture');
+    const images = document.querySelector('.preview-item');
+    const img = document.createElement('img');
+    img.classList.add("full-picture-item");
+    src = oldImg.getAttribute('src');
+    if (src == document.querySelector('.first').getAttribute('src')) {
+        src = document.querySelector('.third').getAttribute('src');
+    } else if (src == document.querySelector('.third').getAttribute('src')) {
+        src = document.querySelector('.second').getAttribute('src');
+    } else if (src == document.querySelector('.second').getAttribute('src')) {
+        src = document.querySelector('.first').getAttribute('src');
+    }
+
+
+    if (oldImg) {
+        oldImg.setAttribute('src', src);
+    } else {
+        wrapper.appendChild(leftArrow);
+        wrapper.appendChild(img);
+        wrapper.appendChild(rightArrow);
+        console.log("Picture added");
     }
 }
+
+rightArrowClick = () => {
+    const oldImg = document.querySelector('.full-picture-item');
+    const wrapper = document.querySelector('.full-picture');
+    const images = document.querySelector('.preview-item');
+    const img = document.createElement('img');
+    img.classList.add("full-picture-item");
+    src = oldImg.getAttribute('src');
+    if (src == document.querySelector('.third').getAttribute('src')) {
+        src = document.querySelector('.first').getAttribute('src');
+    } else if (src == document.querySelector('.second').getAttribute('src')) {
+        src = document.querySelector('.third').getAttribute('src');
+    } else if (src == document.querySelector('.first').getAttribute('src')) {
+        src = document.querySelector('.second').getAttribute('src');
+    }
+
+
+    if (oldImg) {
+        oldImg.setAttribute('src', src);
+    } else {
+        wrapper.appendChild(leftArrow);
+        wrapper.appendChild(img);
+        wrapper.appendChild(rightArrow);
+        console.log("Picture added");
+    }
+
+
+
+}
+
+//#2
+
